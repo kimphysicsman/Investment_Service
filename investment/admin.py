8f0a928ae2c988085a141e2131d0349657190bbc
DongwooKim <kimphysicsman@gmail.com>
2022-09-21 12:09:12 +0900

    feat/ 투자 관련 모델링

--- investment/admin.py
+++ investment/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from investment.models import (
+    Bank as BankModel,
+    Investment as InvestmentModel,
+    AssetGroup as AssetGroupModel,
+    Stock as StockModel,
+    InvestmentStock as InvestmentStockModel
+)
+
+admin.site.register(BankModel)
+admin.site.register(InvestmentModel)
+admin.site.register(AssetGroupModel)
+admin.site.register(StockModel)
+admin.site.register(InvestmentStockModel)
--- investment/models.py
+++ investment/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+from user.models import User as UserModel
+
+
+class Bank(models.Model):
+    """
+        증권사 오브젝트        
+    """
+    name = models.CharField("증권사명", max_length=128)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+
+class Investment(models.Model):
+    """
+        투자(계좌) 오브젝트
+    """
+
+    account_num_validator = RegexValidator(regex = r'^\d+$')
+
+    user = models.ForeignKey(UserModel, verbose_name="고객", on_delete=models.CASCADE)
+    bank = models.ForeignKey(Bank, verbose_name="증권사", on_delete=models.CASCADE)
+
+    account_name = models.CharField("계좌명", max_length=128)
+    account_num = models.CharField("계좌번호", max_length=13, validators=[account_num_validator])
+
+    starting_fund = models.PositiveBigIntegerField("투자원금")
+    total_asset = models.PositiveBigIntegerField("계좌총자산")
+
+    def __str__(self):
+        return f"{self.id} - {self.user.username}'s investment"
+
+
+class AssetGroup(models.Model):
+    """
+        자산그륩 오브젝트
+    """
+
+    name = models.CharField("자산그륩명", max_length=128)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+
+class Stock(models.Model):
+    """
+        종목 오브젝트
+    """
+
+    asset_group = models.ForeignKey(AssetGroup, verbose_name="자산그륩", on_delete=models.CASCADE)
+
+    name = models.CharField("종목명", max_length=128)
+    current_price = models.PositiveIntegerField("현재가")
+    isin = models.CharField("ISIN", max_length=128)
+    
+    insvestment = models.ManyToManyField(Investment, verbose_name="보유종목", related_name="stock", through="InvestmentStock")
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+
+class InvestmentStock(models.Model):
+    investment = models.ForeignKey(Investment, verbose_name="투자", on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, verbose_name="보유종목", on_delete=models.CASCADE)
+
+    order = models.PositiveIntegerField("보유수량")
